07437a40336895d1c9db88c9e8a8644ed4f3dcc7
Michael Justin Barcenas <imaqtchael@gmail.com>
2022-11-24 15:34:42 +0800

    JSONToTable is now working

--- onlineBooking/guest.js
+++ onlineBooking/guest.js
@@ -1,93 +1,3 @@
-window.onload = CheckForPreviousEntry();
-
-function startUp() {
-    updateList();
-    changeBackground();
-}
-
-function changeBackground() {
-    var number = 0;
-    var images = ['basketball.jpg', 'birthday.jpg', 'concert.jpg', 'debut.jpg', 'wedding.jpg'];
-    setInterval(() => {
-        var random = Math.floor(Math.random() * 4);
-        $('#vary').css('background-image', `url(pictures/index/${images[number]})`);
-        number += 1;
-        if (number > 4) {
-            number = 0;
-        }
-        $('#vary').css('transition', '0.9s');
-    }, 3000);
-}
-
-function updateList() {
-    var eventsList;
-    jQuery.ajax({
-        type: "POST",
-        url: 'includes/functions.php',
-        dataType: 'json',
-        data: { functionname: 'getAllList' },
-
-        success: function(obj, textstatus) {
-            eventsList = obj.result;
-            display(eventsList);
-        }
-    });
-}
-
-function searchEvent() {
-    var eventsList;
-    var search = document.getElementById("searchEventSearch").value;
-    var type = document.getElementById("course").value;
-    jQuery.ajax({
-        type: "POST",
-        url: 'includes/functions.php',
-        dataType: 'json',
-        data: { functionname: 'getThis', arguments: [type, search] },
-
-        success: function(obj, textstatus) {
-            eventsList = obj.result;
-            removeData();
-            display(eventsList);
-        }
-    });
-}
-
-function removeData() {
-    var body = document.getElementById("tbody");
-    body.parentNode.removeChild(body);
-}
-
-function display(data) {
-    var events = document.getElementById("events");
-
-    var body = document.createElement("TBODY");
-    body.setAttribute("id", "tbody");
-
-    for (var i = 0; i < data.length; i++) {
-        var tablerow = body.insertRow(-1);
-        var tableCell = tablerow.insertCell(-1);
-        tableCell.innerHTML = data[i].name;
-
-        var tableCell1 = tablerow.insertCell(-1);
-        tableCell1.innerHTML = data[i].time;
-
-        var tableCell2 = tablerow.insertCell(-1);
-        tableCell2.innerHTML = data[i].due;
-
-        var tableCell3 = tablerow.insertCell(-1);
-        tableCell3.innerHTML = data[i].price;
-    }
-    events.appendChild(body);
-}
-
-function loadCSS() {
-    var head = document.getElementsByTagName('HEAD')[0];
-    var link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.href = 'index.css';
-    head.appendChild(link);
-}
-
 var hasAdded = false;
 
 function ReEditTable() {
@@ -197,21 +107,20 @@ function CheckForPreviousEntry() {
 
     console.log(JSON.parse(backup));
 
-    if (backup != null) {
-        alert("may laman");
+    if (backup != null && Object.keys(JSON.parse(backup)).length > 0) {
         JSONToTable(JSON.parse(backup));
         //ReEditTable();
     } else {
-        alert("walang laman");
         return false;
     }
 }
 
 function JSONToTable(json) {
     json.forEach(function(obj) {
-        alert(obj.name);
-        $("#events tbody").append("<tr class='row'><td>" + 3 + "</td><td>" + obj.address + "</td><td>" + obj.email + "</td><td>" + obj.number + "</td><td isButton='delete'><input class='delete-btn' type='submit' value='DELETE'></input></td></tr>");
+        $("#events tbody").append("<tr class='row'><td>" + obj.name + "</td><td>" + obj.address + "</td><td>" + obj.email + "</td><td>" + obj.number + "</td><td isButton='delete'><input class='delete-btn' type='submit' value='DELETE'></input></td></tr>");
     });
+    document.getElementById('submit-btn').style.cssText = 'display: inline;'
+    hasAdded = true;
 }
 
 
@@ -228,6 +137,10 @@ $(document).ready(function() {
             document.getElementById('submit-btn').style.cssText = 'display: none;'
             hasAdded = false;
         }
+
+        var json = JSON.stringify(TableToJSON(document.getElementById("events")));
+
+        DownloadJSON(json);
     });
 
     $(document).on('click', '.add-user', function() {
@@ -274,6 +187,8 @@ $(document).ready(function() {
         //$("#email").val("");
         //$("#number").val("");
     });
+
+    CheckForPreviousEntry();
 
     //changes the picture besides the guest data entry
     var counter = 1;
